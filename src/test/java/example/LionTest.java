bfef3bb4b3a0d0bb3ae24124650e86c7a7723f12
Denis Tonkonogov <denis1199t@yandex.ru>
2023-03-03 19:06:23 +0300

    Commit

---
+++ src/test/java/example/LionTest.java
@@ -0,0 +1,51 @@
+package example;
+
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+
+    @Mock
+    Feline feline;
+    @Test(expected = Exception.class)
+    public void LionConstructorWrongSexTest() throws Exception {
+        Lion lion = new Lion("Wrong", feline);
+    }
+
+    @Test
+    public void getKittens() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        int actual = lion.getKittens();
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void getFood() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> actual = lion.getFood();
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void returnsGender() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        boolean actual = lion.doesHaveMane();
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+
+}
